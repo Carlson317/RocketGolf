e4e3691e84f13d0ee85ca67a1658b2a8c4eb71c5
Carlson317 <cpcarlson95@gmail.com>
2016-04-27 21:11:27 -0600

    Adding mobile controls

--- Roll-A-Ball-Unity/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Roll-A-Ball-Unity/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -15,6 +15,7 @@ namespace UnityStandardAssets.Vehicles.Car
 		public Text golfText;
 		public Text winText;
 		private int count, golfCount;
+		private Vector2 touchOrigin = -Vector2.one;
 		void Start(){
 			count = 0;
 			golfCount = 0;
@@ -39,6 +40,61 @@ namespace UnityStandardAssets.Vehicles.Car
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
             m_Car.Move(h, v, v, handbrake);
 #else
+			//Check if Input has registered more than zero touches
+			if (Input.touchCount > 0)
+			{
+				//Store the first touch detected.
+				Touch myTouch = Input.touches[0];
+
+				//Check if the phase of that touch equals Began
+				if (myTouch.phase == TouchPhase.Began)
+				{
+					//If so, set touchOrigin to the position of that touch
+					touchOrigin = myTouch.position;
+				}
+
+				//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+				else if ((myTouch.phase == TouchPhase.Moved || myTouch.phase == TouchPhase.Stationary) && touchOrigin.x >= 0)
+				{
+					//Set touchEnd to equal the position of this touch
+					Vector2 touchEnd = myTouch.position;
+					//Calculate the difference between the beginning and end of the touch on the x axis.
+					//float x = touchEnd.x - touchOrigin.x;
+
+					//Calculate the difference between the beginning and end of the touch on the y axis.
+					//float y = touchEnd.y - touchOrigin.y;
+
+					//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+					//touchOrigin.x = -1;
+
+					//h = x > 0 ? 1 : -1;
+					//v = y > 0 ? 1 : -1;
+					if(touchEnd.x > (Screen.width * 0.66)){
+						h = 1;
+					}
+					else if (touchEnd.x < (Screen.width * 0.33))
+					{
+						h = -1;
+					}
+					else
+					{
+						h = 0;
+					}
+					if(touchEnd.y >= (Screen.height * 0.5)){
+						v = 1;
+					}
+					else
+					{
+						v = -1;
+					}
+
+					//touchOrigin = touchEnd;
+				}
+				else if (myTouch.phase == TouchPhase.Ended)
+				{
+					touchOrigin.x = -1;
+				}
+			}
             m_Car.Move(h, v, v, 0f);
 #endif
         }
